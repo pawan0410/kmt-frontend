650f0e5c3bd236ae9ed8be0aba7316340c30759a
Karim Boukabbouz <karim@ibs-na.com>
2016-12-01 00:42:31 +0000

    [IMP] Added a debugging tool.

---
+++ src/helpers.js
@@ -0,0 +1,29 @@
+/* © AIG Business. See LICENSE file for full copyright & licensing details. */
+
+/**
+ * Console log with context and pretty colors.
+ * @function
+ * @name prettyConsoleLog
+ * @param {String} message Message to send to the console.
+ * @param {String} context User provided, used to know where the log came from.
+ * @param {Number} colorId To colors the context.
+ * @returns {Boolean} return true if the log was posted, false if not.
+ */
+function prettyConsoleLog( message, context, colorId ) {
+    if( context === -1 )
+
+        /* eslint-disable no-console */
+        return console.log( message );
+
+    let colors = [ "#0097ef", "#ff0055" ],
+        color = colorId ? colors[ colorId ] : colors[ 0 ];
+
+    /* eslint-disable no-console */
+    console.log(
+        "%c%s: %c%s", "color: " + color + ";font-weight: bold;",
+        context || "GLOBAL",
+        "color: black;", message
+    );
+}
+
+export default prettyConsoleLog;
