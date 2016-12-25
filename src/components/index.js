0b20deb563b45a04f552e7d9cff0afff0412f152
Karim Boukabbouz <karim@ibs-na.com>
2016-12-25 13:37:16 +0000

    [IMP] grouped componenets in index.js + added modal sub componenet.

---
+++ src/components/index.js
@@ -0,0 +1,11 @@
+/* © AIG Business. See LICENSE file for full copyright & licensing details. */
+
+import Documents from "./Documents";
+import Login from "./Login";
+import Processor from "./Processor";
+
+export {
+    Documents,
+    Login,
+    Processor
+};
